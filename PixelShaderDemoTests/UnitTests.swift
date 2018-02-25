d9316de7d6f41cfcc26846bae77adba97a28120f
Astemir Eleev <astemireleev@gmail.com>
2018-02-25 15:55:04 +0300

    Renamed UI and unit tests

--- PixelShaderDemoTests/UnitTests.swift
+++ PixelShaderDemoTests/UnitTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import PixelShaderDemo
 
-class PixelShaderDemoTests: XCTestCase {
+class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
--- PixelShaderDemoUITests/UITests.swift
+++ PixelShaderDemoUITests/UITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class PixelShaderDemoUITests: XCTestCase {
+class UITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
